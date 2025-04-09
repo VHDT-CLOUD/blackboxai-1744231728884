54c086270e865bca809016eba55185278dd0c7e0
VHDT <127493725+VHDT-CLOUD@users.noreply.github.com>
2025-04-10 02:19:25 +0530

    Add backend/records/apps.py 1744231763937

---
+++ backend/records/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+class RecordsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'records'
+
+    def ready(self):
+        # Import signals when the app is ready
+        import records.signals
